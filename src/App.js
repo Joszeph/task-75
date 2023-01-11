28c5273830870a24693ef8bcab5834661fe7b28b
Yosif Georgiev <iodesign@live.com>
2023-01-11 19:01:44 +0200

    FIX: adding to local storage

--- src/App.js
+++ src/App.js
@@ -2,31 +2,39 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [text, setText] = useState(localStorage.getItem('text') || "");
+  const [text, setText] = useState(localStorage.getItem("text") || "");
 
-const saveText = () =>{
-  localStorage.getItem('text', text)
-}
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
 
-const handleChange = (e) =>{
-  e.preventDefault()
-  setText(e.target.value)
-}
+  const saveText = () => {
+    localStorage.setItem("Text node:", text);
+  };
 
   return (
     <div className="App">
       <div className="box">
         <div className="field">
           <div className="control">
-            <textarea 
-            onChange={handleChange}
-            value={text}
-            className="textarea is-large" 
-            placeholder="Notes..." />
+            <textarea
+              onChange={handleChange}
+              value={text}
+              className="textarea is-large"
+              placeholder="Notes..."
+            />
           </div>
         </div>
-        <button className="button is-large is-primary is-active" type="submit" onClick={saveText}>Save</button>
-        <button className="button is-large" onClick={()=>setText("")}>Clear</button>
+        <button
+          className="button is-large is-primary is-active"
+          type="submit"
+          onClick={saveText}
+        >
+          Save
+        </button>
+        <button className="button is-large" onClick={() => setText("")}>
+          Clear
+        </button>
       </div>
     </div>
   );
